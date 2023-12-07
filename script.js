019e4bcb87dfe7ed79cd14292b03bf70c9a99283
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-07 14:24:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-<script>
 function functionName() {
   alert("functionNmae");
 }
 	function Acciojob() {
   alert("Acciojob");
 }
-</script>
+functionName();
+Acciojob();
